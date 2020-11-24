3992d6b548aee6fe8a4d2c649bfec97e170af16e
oscar Nam <nyona77@gmail.com>
2020-11-24 19:12:52 +0100

    Initial commit

---
+++ src/main/java/com/udemy/netflix_backend/my_movies_platform/MyMoviesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.netflix_backend.my_movies_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviesPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/netflix_backend/my_movies_platform/MyMoviesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.netflix_backend.my_movies_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
